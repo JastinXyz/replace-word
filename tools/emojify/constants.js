dc9edb9f2a5d9555e5b0f554e7da403133e78752
JstnLT <jastinlinggar@gmail.com>
2022-03-02 18:02:00 +0700

    yo

---
+++ tools/emojify/constants.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.inappropriateEmojis = exports.commonWords = void 0;
+exports.commonWords = new Set([
+    'a',
+    'an',
+    'as',
+    'is',
+    'if',
+    'of',
+    'the',
+    'it',
+    'its',
+    'or',
+    'are',
+    'this',
+    'with',
+    'so',
+    'to',
+    'at',
+    'was',
+    'and',
+]);
+exports.inappropriateEmojis = [
+    '🍆',
+    '💦',
+    '🍑',
+    '🌮',
+    '👅',
+    '🐍',
+    '🔯',
+    '🖕',
+    '🚬',
+    '💣',
+    '🔫',
+    '🔪',
+    '💊',
+    '💉',
+];
